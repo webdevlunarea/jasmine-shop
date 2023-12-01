98c353b695033ea3c4bec4e2863f24c2ff60798c
Aditya Anugrah <akunmlggnibos@gmail.com>
2023-12-01 08:08:32 +0700

    update tampilan succespay,errorpay,progrespay
    :)

--- app/Views/Pages/errorPay.php
+++ app/Views/Pages/errorPay.php
@@ -2,9 +2,16 @@
 <?= $this->section("content"); ?>
 <div class="konten">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Pembayaran Gagal :(</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h1 class="display-5 mt-5">Pembayaran Gagal</h1>
+                <p class="lead">Maaf, pembayaran Anda tidak berhasil diproses.</p>
+                <i class="bi bi-x-circle text-danger display-1 mt-4 mb-4"></i>
+                <div class="mb-3">
+                    <a href="<?= base_url(); ?>" class="btn btn-primary btn-lg me-3">Kembali ke Halaman Utama</a>
+                    <a href="https://wa.me/6281234567890?text=Halo%20,%20saya%20mengalami%20masalah%20dengan%20pembayaran%20saya.%20Bisakah%20Anda%20bantu%20saya?"
+                        class="btn btn-success btn-lg">Butuh Bantuan?</a>
+                </div>
             </div>
         </div>
     </div>
--- app/Views/Pages/successPay.php
+++ app/Views/Pages/successPay.php
@@ -2,11 +2,16 @@
 <?= $this->section("content"); ?>
 <div class="konten">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Pembayaran Berhasil :)</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
                 <p><?= $ceking; ?></p>
                 <p><?= $keranjang; ?></p>
+                <h1 class="display-5 mt-5">Pembayaran Berhasil</h1>
+                <p class="lead">Terima kasih telah melakukan pembayaran.</p>
+                <i class="bi bi-check-circle text-success display-1 mt-4 mb-4"></i>
+                <div class="mb-3">
+                    <a href="<?= base_url(); ?>" class="btn btn-primary btn-lg me-3">Kembali ke Halaman Utama</a>
+                </div>
             </div>
         </div>
     </div>
