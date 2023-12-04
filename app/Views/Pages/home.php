f169a807764fb2338fd9dd7517d4a28e8e135981
galihsuks <galih8.4.2001@gmail.com>
2023-12-04 10:24:04 +0700

    update grid dan navbar

--- app/Views/Pages/home.php
+++ app/Views/Pages/home.php
@@ -4,12 +4,9 @@
     <div class="container">
         <div id="carouselExampleIndicators" class="carousel slide">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner" style="border-radius: 1.5em;">
                 <div class=" carousel-item active">
@@ -22,13 +19,11 @@
                     <img src="img/promo3.webp" class="d-block w-100" alt="...">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -39,28 +34,28 @@
         <h1 class="mb-3">Produk Baru</h1>
         <div class="card-group1">
             <?php foreach ($produk as $p) { ?>
-            <a class="card1" href="/product/<?= $p['id']; ?>">
-                <?php if ($p['diskon']) { ?>
-                <p class="diskon">-<?= $p['diskon']; ?>%</p>
-                <?php } ?>
-                <img src="data:image/jpeg;base64,<?= base64_encode($p['gambar']); ?>" alt="">
-                <div class="mt-3">
-                    <h5 class="mb-0"><?= $p['nama']; ?></h5>
+                <a class="card1" href="/product/<?= $p['id']; ?>">
                     <?php if ($p['diskon']) { ?>
-                    <p class="mb-0 harga d-inline">Rp
-                        <?php
+                        <p class="diskon">-<?= $p['diskon']; ?>%</p>
+                    <?php } ?>
+                    <img src="data:image/jpeg;base64,<?= base64_encode($p['gambar']); ?>" alt="">
+                    <div class="mt-3">
+                        <h5 class="mb-0"><?= $p['nama']; ?></h5>
+                        <?php if ($p['diskon']) { ?>
+                            <p class="mb-0 harga d-inline">Rp
+                                <?php
                                 $persen = (100 - $p['diskon']) / 100;
                                 $hasil = $persen * $p['harga'];
                                 echo number_format($hasil, 0, ",", ".");
                                 ?></p>
-                    <p class="mb-0 d-inline" style="text-decoration: line-through; font-size: small; color: grey;">Rp
-                        <?= number_format($p['harga'], 0, ",", "."); ?></p>
-                    <?php } else { ?>
-                    <p class="mb-0 harga">Rp <?= number_format($p['harga'], 0, ",", "."); ?></p>
-                    <?php } ?>
-                    <p>★★★☆☆ (<?= $p['rate']; ?>)</p>
-                </div>
-            </a>
+                            <p class="mb-0 d-inline" style="text-decoration: line-through; font-size: small; color: grey;">Rp
+                                <?= number_format($p['harga'], 0, ",", "."); ?></p>
+                        <?php } else { ?>
+                            <p class="mb-0 harga">Rp <?= number_format($p['harga'], 0, ",", "."); ?></p>
+                        <?php } ?>
+                        <!-- <p>★★★☆☆ (<?= $p['rate']; ?>)</p> -->
+                    </div>
+                </a>
             <?php } ?>
         </div>
         <div class="mx-auto mt-2" style="width: fit-content;">
@@ -117,7 +112,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -125,7 +120,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -133,7 +128,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -141,7 +136,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -149,7 +144,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -157,7 +152,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
             <div class="card1">
@@ -165,7 +160,7 @@
                 <div class="mt-3">
                     <h5 class="mb-0">JOYSTICK H-92 New!</h5>
                     <p class="mb-0 harga">Rp 50.000</p>
-                    <p>★★★☆☆ (100)</p>
+                    <!-- <p>★★★☆☆ (100)</p> -->
                 </div>
             </div>
         </div>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -3,8 +3,7 @@
         <a class="navbar-brand" href="/" style="font-weight: bold;">
             <img src="../img/Logo Jasmine.png" height="30em">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -18,28 +17,30 @@
                 <li class="nav-item">
                     <a class="nav-link <?= $title == 'Tentang' ? "active " : ""; ?>" href="/about">Tentang</a>
                 </li>
-                <?php if (!session()->get('isLogin')) { ?>
                 <li class="nav-item">
-                    <a class="nav-link <?= $title == 'Daftar' ? "active " : ""; ?>" href="/login">Masuk</a>
+                    <a class="nav-link <?= $title == 'Semua Produk' ? "active " : ""; ?>" href="/all">Produk</a>
                 </li>
+                <?php if (!session()->get('isLogin')) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link <?= $title == 'Daftar' ? "active " : ""; ?>" href="/login">Masuk</a>
+                    </li>
                 <?php } ?>
             </ul>
             <form class="d-flex" role="search" id="search-box">
                 <div class="input-group">
-                    <input id="search-input" type="text" class="form-control" placeholder="Search" aria-label="Search"
-                        aria-describedby="button-addon2">
+                    <input id="search-input" type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
                     <button class="btn btn-dark" type="submit"><i class="material-icons">search</i></button>
                 </div>
             </form>
             <?php if (session()->get('isLogin')) { ?>
-            <?php if (session()->get('role') == 0 ) { ?>
-            <a href="/wishlist" class="btn"><i class="material-icons">favorite_border</i></a>
-            <a href="/cart" class="btn"><i class="material-icons">shopping_cart</i></a>
-            <a href="/account" class="btn"><i class="material-icons">person_outline</i></a>
-            <?php } else { ?>
-            <a href="/listproduct" class="btn"><i class="material-icons">view_list</i></a>
-            <a href="/account" class="btn"><i class="material-icons">person_outline</i></a>
-            <?php } ?>
+                <?php if (session()->get('role') == 0) { ?>
+                    <a href="/wishlist" class="btn"><i class="material-icons">favorite_border</i></a>
+                    <a href="/cart" class="btn"><i class="material-icons">shopping_cart</i></a>
+                    <a href="/account" class="btn"><i class="material-icons">person_outline</i></a>
+                <?php } else { ?>
+                    <a href="/listproduct" class="btn"><i class="material-icons">view_list</i></a>
+                    <a href="/account" class="btn"><i class="material-icons">person_outline</i></a>
+                <?php } ?>
             <?php } ?>
         </div>
     </div>
