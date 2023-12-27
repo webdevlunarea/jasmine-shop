d58c140864fa6abce09a39ba35c6500dd82f51b5
Aditya Anugrah <akunmlggnibos@gmail.com>
2023-12-27 09:02:09 +0700

    update favorite

--- app/Views/Pages/wishlist.php
+++ app/Views/Pages/wishlist.php
@@ -8,32 +8,33 @@
         </div>
         <div class="card-group1 no-scroll">
             <?php if (count($wishlist) > 0) { ?>
-                <?php foreach ($produk as $p) { ?>
-                    <a class="card1" href="/product/<?= $p['id']; ?>">
-                        <?php if ($p['diskon']) { ?>
-                            <p class="diskon">-<?= $p['diskon']; ?>%</p>
-                        <?php } ?>
-                        <img src="data:image/jpeg;base64,<?= base64_encode($p['gambar']); ?>" alt="">
-                        <div class="mt-3">
-                            <h5 class="mb-0"><?= $p['nama']; ?></h5>
-                            <?php if ($p['diskon']) { ?>
-                                <p class="mb-0 harga d-inline">Rp
-                                    <?php
+            <?php foreach ($produk as $p) { ?>
+            <a class="card1" href="/product/<?= $p['id']; ?>">
+                <?php if ($p['diskon']) { ?>
+                <p class="diskon">-<?= $p['diskon']; ?>%</p>
+                <?php } ?>
+                <img src="data:image/jpeg;base64,<?= base64_encode($p['gambar']); ?>" alt="">
+                <div class="mt-3">
+                    <h5 class="mb-0"><?= $p['nama']; ?></h5>
+                    <?php if ($p['diskon']) { ?>
+                    <p class="mb-0 harga d-inline">Rp
+                        <?php
                                     $persen = (100 - $p['diskon']) / 100;
                                     $hasil = $persen * $p['harga'];
                                     echo number_format($hasil, 0, ",", ".");
                                     ?></p>
-                                <p class="mb-0 d-inline" style="text-decoration: line-through; font-size: small; color: grey;">Rp
-                                    <?= number_format($p['harga'], 0, ",", "."); ?></p>
-                            <?php } else { ?>
-                                <p class="mb-0 harga">Rp <?= number_format($p['harga'], 0, ",", "."); ?></p>
-                            <?php } ?>
-                            <p>★★★☆☆ (<?= $p['rate']; ?>)</p>
-                        </div>
-                    </a>
-                <?php } ?>
+                    <p class="mb-0 d-inline" style="text-decoration: line-through; font-size: small; color: grey;">Rp
+                        <?= number_format($p['harga'], 0, ",", "."); ?></p>
+                    <?php } else { ?>
+                    <p class="mb-0 harga">Rp <?= number_format($p['harga'], 0, ",", "."); ?></p>
+                    <?php } ?>
+                    <p>★★★☆☆ (<?= $p['rate']; ?>)</p>
+                </div>
+            </a>
+            <?php } ?>
             <?php } else { ?>
-                <h5>Tidak ada wishlist</h5>
+            <h5>Oops, keranjangmu masih kosong!
+                Sepertinya belum nentuin produk favoritmu ya.</h5>
             <?php } ?>
         </div>
     </div>
