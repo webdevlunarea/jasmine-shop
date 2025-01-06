51f1ae467bcfd734e49329d804a7c2c07b0eb9d7
galihsuks <galih8.4.2001@gmail.com>
2025-01-06 12:05:57 +0700

    kce

--- app/Views/pages/account.php
+++ app/Views/pages/account.php
@@ -20,35 +20,40 @@
             <div>
                 <div class="p-2">
                     <h3>Edit Profilemu</h3>
-                    <p>Email : <?= session()->get('email') ?> </p>
+                    <p>Email : <?= session()->get('email') ?></p>
                     <?php if ($msg) { ?>
                         <div class="alert alert-success" role="alert">
                             <?= $msg; ?>
                         </div>
                     <?php } ?>
-                    <form class="row g-3" action="/account" method="post">
-                        <div class="col-12">
-                            <label for="inputPassword4" class="form-label">Sandi</label>
-                            <input name="sandi" type="password" class="form-control" id="inputPassword4">
-                        </div>
-                        <?php if (session()->get('role') == '0') { ?>
+                    <?php if (session()->get('active')) { ?>
+                        <form class="row g-3" action="/account" method="post">
                             <div class="col-12">
-                                <label for="inputAddress" class="form-label">Nama Lengkap</label>
-                                <input name="nama" type="text" class="form-control" placeholder="Nama Lengkap" value="<?= $nama ?>" required>
+                                <label for="inputPassword4" class="form-label">Sandi</label>
+                                <input name="sandi" type="password" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="col-12">
-                                <label for="inputAddress" class="form-label">Nomor Handphone</label>
-                                <input name="nohp" type="number" class="form-control" placeholder="No HP" value="<?= $nohp ?>" required>
-                            </div>
-                            <div class="col-12">
-                                <label for="inputAddress" class="form-label">Tanggal Lahir</label>
-                                <input name="tgl_lahir" type="date" class="form-control" value="<?= $tgl_lahir ?>" required>
-                            </div>
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary1">Simpan</button>
-                            </div>
-                        <?php } ?>
-                    </form>
+                            <?php if (session()->get('role') == '0') { ?>
+                                <div class="col-12">
+                                    <label for="inputAddress" class="form-label">Nama Lengkap</label>
+                                    <input name="nama" type="text" class="form-control" placeholder="Nama Lengkap" value="<?= $nama ?>" required>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputAddress" class="form-label">Nomor Handphone</label>
+                                    <input name="nohp" type="number" class="form-control" placeholder="No HP" value="<?= $nohp ?>" required>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputAddress" class="form-label">Tanggal Lahir</label>
+                                    <input name="tgl_lahir" type="date" class="form-control" value="<?= $tgl_lahir ?>" required>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary1">Simpan</button>
+                                </div>
+                            <?php } ?>
+                        </form>
+                    <?php } else { ?>
+                        <p class="mb-1 text-secondary">Biodata dapat diubah ketika akun telah diaktifkan</p>
+                        <a href="/verify" class="btn btn-primary1">Aktivasi akun</a>
+                    <?php } ?>
                 </div>
             </div>
         </div>
