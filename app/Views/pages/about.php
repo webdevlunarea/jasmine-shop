60695bf6b25e8d95f269a010d78c428aae22b7dc
galihsuks <galih8.4.2001@gmail.com>
2025-01-13 20:37:48 +0700

    benerin file-file yg hilang

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -68,55 +68,7 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="d-flex flex-wrap justify-content-center gap-3 align-items-center">
-                <div class="kotak-tentang">
-                    <h1 class="mb-0">40+</h1>
-                    <p class="mb-0">Tahun Pengalaman</p>
-                </div>
-                <div class="kotak-tentang">
-                    <h1 class="mb-0">100%</h1>
-                    <p class="mb-0">Produk bergaransi</p>
-                </div>
-                <div class="kotak-tentang">
-                    <h1 class="mb-0">40+</h1>
-                    <p class="mb-0">Tahun Pengalaman</p>
-                </div>
-                <div class="kotak-tentang">
-                    <h1 class="mb-0">40+</h1>
-                    <p class="mb-0">Tahun Pengalaman</p>
-                </div>
-            </div> -->
         </div>
     </div>
-
-    <!-- <span class="garis container show-ke-hide mb-4"></span>
-
-    <div class="baris-ke-kolom-reverse justify-content-center align-content-center container gap-3 pb-5">
-        <div><img src="" alt="" style="width: 100%" /></div>
-        <div class="d-flex flex-column justify-content-center align-content-center">
-            <h2>Visi</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nihil
-                aliquid quae quis, ipsum sapiente quidem dolorum. Libero officiis
-                porro molestiae sapiente eaque molestias deserunt, sed officia
-                corrupti optio corporis?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nihil
-                aliquid quae quis, ipsum sapiente quidem dolorum. Libero officiis
-                porro molestiae sapiente eaque molestias deserunt, sed officia
-                corrupti optio corporis?
-            </p>
-            <h2>Misi</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga porro
-                aliquam eveniet eaque, dignissimos minima. Voluptatum, repellendus et.
-                Sapiente beatae earum eum sed aspernatur optio soluta vel, nesciunt
-                qui! Molestias.
-            </p>
-        </div>
-    </div> -->
 </div>
-<a href="/cekdouble">cek double</a>
-<p>Cek apa <?= session()->get('cekdouble') ? session()->get('cekdouble') : 'Belum'; ?></p>
 <?= $this->endSection(); ?>
